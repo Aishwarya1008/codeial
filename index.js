bdcb33670e96cbfc57416a457dd32a3fb9bdf1ac
Aishwarya <aishwarya.rohatgi1008@gmail.com>
2021-03-26 19:47:42 +0530

    Add Home page i.e. home.ejs

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const port = 8000;
+const path = require('path');
 
 const app = express();
 
 app.use('/', require('./routes/index'));
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.listen(port, function(err){
     if(err){
